bf0f613c6224f8ecb8715b062bcc2398b6a764fa
Erik Steiger <rkstgr@gmail.com>
2023-08-26 11:54:21 +0200

    Add resnet18 results

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+# Benchmark
 Benchmark from https://github.com/kuangliu/pytorch-cifar
+
+Resnet18 (~ 30 Epochs): 93%
--- utils.py
+++ utils.py
@@ -41,8 +41,10 @@ def init_params(net):
             if m.bias:
                 init.constant(m.bias, 0)
 
-
-_, term_width = os.popen('stty size', 'r').read().split()
+try:
+    _, term_width = os.popen('stty size', 'r').read().split()
+except ValueError:
+    term_width = 80
 term_width = int(term_width)
 
 TOTAL_BAR_LENGTH = 65.
